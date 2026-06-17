8b1151eff3bdb7e89722ffde38044f538f5bc1e8
hashinijayarathna2003-beep <hashinijayarathna2003@gmail.com>
2026-06-17 19:38:47 +0530

    feat: rate-limit login to 10 per 15 min

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const { body } = require('express-validator');
+const rateLimit = require('express-rate-limit');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const validate = require('../middleware/validate');
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  handler: (req, res) => {
+    res.status(429).json({
+      code: 429,
+      message: 'Too many attempts, try later'
+    });
+  }
+});
+
 router.post(
   '/login',
+  loginLimiter,
   [
     body('email')
       .isEmail()
