9dda81c0e8f9df0810e1e574c75bbb2f1c3e9171
Praveen Madawalage <praveenrmadawalage@gmail.com>
2026-06-18 10:11:50 +0530

    fix: resolved AppRoutes.jsx location

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-import AppRoutes from './routes/AppRoutes.jsx';
+import AppRoutes from './AppRoutes.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
